dc417c78a1d0eb467bcd60bc4bdcf90aeb9f533f
Francisco Manuel <fsaa93@gmail.com>
2023-06-06 15:04:34 +0200

    TODO list

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/*
+TODO:	Minima Functinonal
+
+	Utilidad botones de navegación
+	Añadir participantes
+	Sustituir participante gandor por winner
+	Control de si no hay participantes
+		-> no possible page en vez de winner page
+	----->>> confeti opcional
+	Giro logo // CSS o JS?
+	Timeout de lottery page
+*/
