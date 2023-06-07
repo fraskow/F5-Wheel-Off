c1a3eeab9cbbaff5d259e92eb1835c8d10efcfc4
snake324 <kristi4n90@gmail.com>
2023-06-07 22:03:09 +0200

    player media-queries

--- public/js/winner.js
+++ public/js/winner.js
@@ -1,6 +1,15 @@
 function mostrar() {
     let ganador = localStorage.getItem("winner");
+    let participantesAnteriores = localStorage.getItem("participants");
+  
     document.getElementById("winner").innerHTML = ganador;
+  
+    if (participantesAnteriores) {
+      localStorage.setItem("participants", participantesAnteriores);
+    }
+    
     localStorage.removeItem("winner");
-}
-mostrar();
+  }
+  
+  mostrar();
+  
